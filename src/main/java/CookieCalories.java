b2ae6eb40af529e0abc9433f1d10a48704a888ad
vueld <vueld@192.168.1.7>
2019-09-07 19:30:56 -0500

    Finished projects

--- src/main/java/CookieCalories.java
+++ src/main/java/CookieCalories.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,6 +15,23 @@ public class CookieCalories
 {
     public static void main(String[] args) 
     {
-        int 
+        int cookieCalories = 75;
+        
+        String cookiesEatenAsString = JOptionPane.showInputDialog(null, "Enter number of cookies eaten:");
+        
+        try
+        {
+        float cookiesEaten = Float.parseFloat(cookiesEatenAsString);
+        } catch (Exception x)
+        {
+            JOptionPane.showMessageDialog(null, "Amount not entered as number");
+            System.exit(1);
+        }
+        
+        float cookiesEaten = Float.parseFloat(cookiesEatenAsString);
+        
+        float caloriesConsumed = cookiesEaten * cookieCalories;
+        
+        System.out.printf("Total Calories Consumed:\t%.2f", caloriesConsumed);
     }
 }
